9db5ce684e7d86ed79214dd3a9549108fd80f5c0
Qjten0vE9P2vIsv3 <tekinselim.57.57@gmail.com>
2025-05-14 00:55:19 +0300

    IdentityServer

---
+++ IdentityServer/Inventa.IdentityServer/Controllers/RegistersController.cs
@@ -0,0 +1,44 @@
+﻿using Inventa.IdentityServer.Dtos;
+using Inventa.IdentityServer.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace Inventa.IdentityServer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RegistersController : ControllerBase
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        public RegistersController(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+        [HttpPost]
+        public async Task<IActionResult> Register(UserRegisterDto userRegisterDto)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            var user = new ApplicationUser
+            {
+                UserName = userRegisterDto.Username,
+                Email = userRegisterDto.Email,
+                Name = userRegisterDto.Name,
+                Surname = userRegisterDto.Name
+            };
+            var result = await _userManager.CreateAsync(user, userRegisterDto.Password);
+            if (result.Succeeded)
+            {
+                return Ok(new { Message = "User registered successfully" });
+            }
+            else
+            {
+                return Ok("Bir hata oluştutekrar deneyiniz");
+            }
+        }
+    }
+}
